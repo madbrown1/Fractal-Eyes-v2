2f6f788790057d6584a77de3a56d446ed9dc8950
shaunmb98 <48534742+shaunmb98@users.noreply.github.com>
2021-02-16 17:02:07 -0700

    Updated Main

--- ALLTOGETHER/main.py
+++ ALLTOGETHER/main.py
@@ -3,13 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+import sys
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi("Ex_GUI.ui", self)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('from Shaun w love')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
